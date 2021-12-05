dead88b4ce133d54836c413d9adc0388476ef8e5
AlinaKononova <54644298+AlinaKononova@users.noreply.github.com>
2021-12-05 20:24:41 +0200

    Class Animal/Cat/Dog were filled in

--- src/main/java/oop_practice/cat_dog/animal/Animal.java
+++ src/main/java/oop_practice/cat_dog/animal/Animal.java
@@ -2,4 +2,8 @@ package oop_practice.cat_dog.animal;
 
 public class Animal {
 
+  public void sound() {
+    System.out.println("It's an Animal");
+  }
+
 }
--- src/main/java/oop_practice/cat_dog/cat/Cat.java
+++ src/main/java/oop_practice/cat_dog/cat/Cat.java
@@ -1,4 +1,17 @@
 package oop_practice.cat_dog.cat;
 
-public class Cat {
+import oop_practice.cat_dog.animal.Animal;
+
+public class Cat extends Animal {
+  private String name;
+
+  public Cat(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public void sound() {
+    System.out.println("I'am a Cat. My name " + name + ". Mew");
+  }
+
 }
